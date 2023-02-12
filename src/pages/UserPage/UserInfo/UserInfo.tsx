ff17b0b779357c0e4c6a4ba915121afd0f5ebf9e
Aleksandr Yermolaev <sanyagt50@gmail.com>
2023-02-12 11:31:23 +0300

    feature/#26 - feat: add user context to user page

--- src/pages/UserPage/UserInfo/UserInfo.tsx
+++ src/pages/UserPage/UserInfo/UserInfo.tsx
@@ -1,20 +1,10 @@
 import Avatar from '../../../components/avatar';
 import TextParagraph from '../../../components/paragraph/TextParagraph';
+import { useUser } from '../../../hooks/useUser';
 import classes from './UserInfo.module.css';
 
 const UserInfo = () => {
-  const user = {
-    id: '63dce6b73d2c466b038fc8a9',
-    name: 'Aleksandr Yermolaev',
-    email: 'user1@example.com',
-    image: 'https://avatars3.githubusercontent.com/u/9384699?s=400&v=4',
-    age: 25,
-    location: 'Kyiv',
-    followers: ['63dcd6fa9c1a365e8cf6fdf3', '63dcd7599c1a365e8cf6fdf5'],
-    password: '',
-    about:
-      'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
-  };
+  const { user } = useUser();
 
   return (
     <div className={classes.wrapper}>
@@ -36,7 +26,7 @@ const UserInfo = () => {
         >
           {user.name.split(' ')[1]}
         </TextParagraph>
-        <TextParagraph>{`${user.age} y.o`}</TextParagraph>
+        <TextParagraph>{user.age ? `${user.age} y.o` : ''}</TextParagraph>
         <TextParagraph>{user.location}</TextParagraph>
         <TextParagraph>{user.email}</TextParagraph>
       </div>
--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -5,28 +5,17 @@ import { TypePost, TypeUser } from '../../types/types';
 import UserInfo from './UserInfo/UserInfo';
 import Post from '../../components/thread-posts/post';
 import classes from './UserPage.module.css';
+import { useUser } from '../../hooks/useUser';
 
 const UserPage = () => {
-  // TODO: get this user data from global storage
-  const user = {
-    id: '63dce6b73d2c466b038fc8a9',
-    name: 'Aleksandr Yermolaev',
-    email: 'user1@example.com',
-    image: 'https://avatars3.githubusercontent.com/u/9384699?s=400&v=4',
-    age: 25,
-    location: 'Kyiv',
-    followers: ['63dcd6fa9c1a365e8cf6fdf3', '63dcd7599c1a365e8cf6fdf5'],
-    password: '',
-    about:
-      'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
-  };
+  const { user } = useUser();
 
   const apiService = new ApiService();
   const [posts, setPosts] = useState<[TypePost] | null>(null);
 
   useEffect(() => {
     async function getUserPosts() {
-      const userPosts = await apiService.getUserPosts(user.id);
+      const userPosts = await apiService.getUserPosts(user._id);
       setPosts(userPosts);
     }
     void getUserPosts();
@@ -54,7 +43,7 @@ const UserPage = () => {
     <div className={classes.wrapper}>
       <UserInfo />
       <br />
-      <TextParagraph weight='bold'>About</TextParagraph>
+      {user.about ? <TextParagraph weight='bold'>About</TextParagraph> : null}
       <TextParagraph size='small'>{user.about}</TextParagraph>
       <br />
       {posts ? renderPosts(posts) : null}
